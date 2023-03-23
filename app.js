2d1a51a2079c8d4d5f49daafe865800feca351a8
BurimJ <ipyroburnz@gmail.com>
2023-03-23 13:47:14 +0100

    n

--- app.js
+++ app.js
@@ -3,6 +3,49 @@
 // ============ LOAD & INIT APP ============ //
 window.addEventListener("load", initApp);
 
+async function initApp() {
+  const harry = await getCharacter(
+    "https://raw.githubusercontent.com/cederdorff/dat-js/main/data/harry.json"
+  );
+  showCharacter(harry);
+  const ron = await getCharacter(
+    "https://raw.githubusercontent.com/cederdorff/dat-js/main/data/ron.json"
+  );
+  showCharacter(ron);
+  const hermione = await getCharacter(
+    "https://raw.githubusercontent.com/cederdorff/dat-js/main/data/hermione.json"
+  );
+  showCharacter(hermione);
+  const draco = await getCharacter(
+    "https://raw.githubusercontent.com/cederdorff/dat-js/main/data/draco.json"
+  );
+  showCharacter(draco);
+  const snape = await getCharacter(
+    "https://raw.githubusercontent.com/cederdorff/dat-js/main/data/severus.json"
+  );
+  showCharacter(snape);
+}
+
+async function getCharacter(url) {
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
+}
+
+async function showCharacter(character) {
+  console.log(character);
+  document.querySelector("body").insertAdjacentHTML(
+    "beforebegin",
+    /*html*/ `
+  <article>
+  <img src="${character.image}" />
+     <h2>${character.name}</h2>
+  </article> 
+  `
+  );
+}
+
+/*
 function initApp() {
     // Harry Potter
     const potterName = "Harry Potter";
@@ -27,3 +70,4 @@ function initApp() {
         potterImage
     );
 }
+*/
